478299d8eaa1405f505992fc6f2456eb598520df
soeter01 <50205733+soeter01@users.noreply.github.com>
2024-10-22 16:45:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='day_ahead_opt',
     version='2024.8.4',
     packages=['dao', 'dao.prog', 'dao.webserver', 'dao.webserver.app'],
-    url='https://github.com/corneel27/day-ahead',
+    url='https://github.com/soeter01/day-ahead',
     license='Apache License, Version 2.0',
     author='Cees van Beek',
     author_email='cees.van.beek@xs4all.nl',
