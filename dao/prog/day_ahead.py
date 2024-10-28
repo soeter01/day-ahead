1215d4b0eadf031873ad975411c4dd9be37c1489
soeter01 <50205733+soeter01@users.noreply.github.com>
2024-10-28 17:10:20 +0100

    Update day_ahead.py

--- dao/prog/day_ahead.py
+++ dao/prog/day_ahead.py
@@ -270,6 +270,8 @@ class DaCalc(DaBase):
                 logging.info(f"Start waarden: \n{start_df.to_string()}")
         except Exception as ex:
             logging.error(ex)
+        while len(b_l) < len(uur):  #addition from Cees ivm DST
+          b_l.append(b_l[-1])
         logging.debug(f"lengte prognose arrays:")
         logging.debug(f"uur: {len(uur)}")
         logging.debug(f"tijd: {len(tijd)}")
